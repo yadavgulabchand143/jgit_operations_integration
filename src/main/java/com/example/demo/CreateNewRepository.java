147d991b335c3c7141917c03592469b7e30c87af
Administrator <Administrator@172.31.45.55>
2022-02-18 10:58:43 +0000

    remove files

--- src/main/java/com/example/demo/CreateNewRepository.java
+++ src/main/java/com/example/demo/CreateNewRepository.java
@@ -5,20 +5,29 @@ import java.io.IOException;
 
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.lib.Repository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CreateNewRepository {
 
+	@Autowired 
+	Environment environment;
+	
 	@Value("${git.local.dir}")
-	private static String localDirPath;
+	private String localDirPath ;
+	
 
 	public Repository createRepositoryInLocal() throws IOException {
 		System.out.println("BEGIN ::: Inside  createRepositoryInLocal() method of CreateNewRepository");
 		Repository repository = null;
 		try {
 			// prepare a new folder
+			if(localDirPath == null) {
+				localDirPath = environment.getProperty("git.local.dir");
+			}
 			File localPath = new File(localDirPath);
 			if (localPath.exists()) {
 				localPath.mkdir();
--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,14 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
-@SpringBootApplication(scanBasePackages="com.example.demo")
+@SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context =   SpringApplication.run(DemoApplication.class, args);
 		EntryPoint entryPoint = context.getBean(EntryPoint.class);
-		entryPoint.pushToRemote("handle Exception", "yadavgulabchand143", "develop");
-		
+		//entryPoint.pushToRemote("Initial Commit", "yadavgulabchand143", "master");
+		entryPoint.createRepository();
+		//entryPoint.cloneRepository();
+		//entryPoint.createBranch("master", "develop");
+		//entryPoint.pullFromRemote();
+		//entryPoint.getCurrentBranchDetails();
+		//entryPoint.switchBranch("master");
 	}
 
 }
